590ad7d6abbd0727fbd6ae72ada4cb4bf7fb541f
sriramsenthill <sriram.senthilnathan@gmail.com>
2024-07-04 11:30:19 +0530

    first commit

---
+++ src/components/layout/horizontal/VerticalNavContent.jsx
@@ -0,0 +1,51 @@
+// Next Imports
+import Link from 'next/link'
+import { useParams } from 'next/navigation'
+
+// Third-party Imports
+import PerfectScrollbar from 'react-perfect-scrollbar'
+
+// Component Imports
+import NavHeader from '@menu/components/vertical-menu/NavHeader'
+import Logo from '@components/layout/shared/Logo'
+import NavCollapseIcons from '@menu/components/vertical-menu/NavCollapseIcons'
+
+// Hook Imports
+import useHorizontalNav from '@menu/hooks/useHorizontalNav'
+
+// Util Imports
+import { mapHorizontalToVerticalMenu } from '@menu/utils/menuUtils'
+import { getLocalizedUrl } from '@/utils/i18n'
+
+const VerticalNavContent = ({ children }) => {
+  // Hooks
+  const { isBreakpointReached } = useHorizontalNav()
+  const { lang: locale } = useParams()
+
+  // Vars
+  const ScrollWrapper = isBreakpointReached ? 'div' : PerfectScrollbar
+
+  return (
+    <>
+      <NavHeader>
+        <Link href={getLocalizedUrl('/', locale)}>
+          <Logo />
+        </Link>
+        <NavCollapseIcons
+          lockedIcon={<i className='tabler-circle-dot text-xl' />}
+          unlockedIcon={<i className='tabler-circle text-xl' />}
+          closeIcon={<i className='tabler-x text-xl' />}
+        />
+      </NavHeader>
+      <ScrollWrapper
+        {...(isBreakpointReached
+          ? { className: 'bs-full overflow-y-auto overflow-x-hidden' }
+          : { options: { wheelPropagation: false, suppressScrollX: true } })}
+      >
+        {mapHorizontalToVerticalMenu(children)}
+      </ScrollWrapper>
+    </>
+  )
+}
+
+export default VerticalNavContent
